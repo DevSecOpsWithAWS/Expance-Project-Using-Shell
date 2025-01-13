451657ed35c74a4a6f13d2162548d6751723737b
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-01-13 14:41:57 +0530

    Database.sh is added

---
+++ database.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+LOG_FOLDER_NAME="/var/log/expance-logs"
+LOG_FILE=$(echo $0 | cut -d '.' -f1)
+TIMESTAMP=$(date +%Y-%m-%d:%H:%M:%S)
+LOG_FILE_NAME="$LOG_FOLDER_NAME/$LOG_FILE/$TIMESTAMP"
+USERID=$(id -u)
+echo "Scripting is executing at --> : $TIMESTAMP" &>>$LOG_FILE_NAME
+
+if[$USERID -ne 0]
+then
+  echo "You don't have access to execute this script"
+  exit 1
+fi
+
+
+
+
+#dnf install mysql-server -y
