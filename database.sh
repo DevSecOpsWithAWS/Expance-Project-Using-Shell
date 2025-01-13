536b706b062a832747ba726150cd71465a1edff0
DevSecOpsWithAWS <choudareddy1234@gmail.com>
2025-01-13 14:45:32 +0530

    Database.sh is added

--- database.sh
+++ database.sh
@@ -6,8 +6,8 @@ TIMESTAMP=$(date +%Y-%m-%d:%H:%M:%S)
 LOG_FILE_NAME="$LOG_FOLDER_NAME/$LOG_FILE/$TIMESTAMP"
 USERID=$(id -u)
 echo "Scripting is executing at --> : $TIMESTAMP" &>>$LOG_FILE_NAME
-
-if[$USERID -ne 0]
+echo "$USERID"
+if [ $USERID -ne 0 ]
 then
   echo "You don't have access to execute this script"
   exit 1
